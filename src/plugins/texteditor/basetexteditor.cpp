56f327b3d0745f7a838494906f39dded0ef278a4
Yuchen Deng <loaden@gmail.com>
2012-09-18 10:40:27 +0800

    Set modified after switch UTF-8 BOM
    Change-Id: I593497ffdd0ecb57bfa886be22220243f020caed

--- src/plugins/texteditor/basetexteditor.cpp
+++ src/plugins/texteditor/basetexteditor.cpp
@@ -5915,6 +5915,8 @@ void BaseTextEditorWidget::circularPaste()
 void BaseTextEditorWidget::switchUtf8bom()
 {
     baseTextDocument()->switchUtf8Bom();
+    if (!document()->isModified())
+        document()->setModified(true);
 }
 
 QMimeData *BaseTextEditorWidget::createMimeDataFromSelection() const
