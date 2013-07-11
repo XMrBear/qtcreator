0a2a4fc1aa5e169aca1c2aced51c80b2a48741ca
Yuchen Deng <loaden@gmail.com>
2013-07-11 21:48:58 +0800

    Introduce CMake build scripts
    Change-Id: I2edd268dbfe46fa12d2587ffb6395fbe0ac68547

---
+++ cmake/QtcPlugin.cmake
@@ -0,0 +1,65 @@
+set(QTC_PLUGIN_PATH lib/qtcreator/plugins/QtProject)
+set(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+macro(qtc_add_plugin target)
+    add_library(${target} SHARED ${ARGN} ${pchSrcVar})
+    qtc_generate_pluginspec(${target})
+    qtc_generate_json(${target})
+    target_include_directories(${target} PRIVATE
+        ${CMAKE_SOURCE_DIR}/src/libs
+        ${CMAKE_SOURCE_DIR}/src/plugins
+    )
+    set_target_properties(${target} PROPERTIES
+        LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}
+        RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}
+        ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
+    )
+    if(WINDOWS)
+        set_property(TARGET ${target} PROPERTY DEBUG_POSTFIX "d")
+    endif()
+    if(MINGW)
+        set_target_properties(${target} PROPERTIES
+            PREFIX ""
+            IMPORT_SUFFIX ".a"
+        )
+    endif()
+    install(TARGETS ${target}
+        LIBRARY DESTINATION ${QTC_PLUGIN_PATH}
+        RUNTIME DESTINATION ${QTC_PLUGIN_PATH}
+        ARCHIVE DESTINATION bin
+    )
+endmacro()
+
+function(qtc_generate_pluginspec target)
+    set(specInFile ${CMAKE_CURRENT_SOURCE_DIR}/${target}.pluginspec.in)
+    set(specFile ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}/${target}.pluginspec)
+    set(txtCmakeLists ${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt)
+    if(${txtCmakeLists} IS_NEWER_THAN ${specFile}
+        OR ${specInFile} IS_NEWER_THAN ${specFile}
+        OR VERSION_UPDATED)
+        file(READ ${specInFile} specInContent)
+        set(dependency "<dependencyList>")
+        foreach(d ${depends})
+            set(dependency "${dependency}\n        <dependency name=\"${d}\" version=\"${QTCREATOR_VERSION}\"/>")
+        endforeach()
+        set(dependency "${dependency}\n    </dependencyList>")
+        string(REPLACE $$QTCREATOR_VERSION ${QTCREATOR_VERSION} specInContent "${specInContent}")
+        string(REPLACE $$dependencyList "${dependency}" specInContent "${specInContent}")
+        string(REPLACE "\\\"" "\"" specInContent "${specInContent}")
+        file(WRITE ${specFile} "${specInContent}")
+    endif()
+    install(FILES ${specFile}
+        DESTINATION ${QTC_PLUGIN_PATH}
+    )
+endfunction()
+
+function(qtc_generate_json target)
+    set(specFile ${CMAKE_BINARY_DIR}/${QTC_PLUGIN_PATH}/${target}.pluginspec)
+    set(jsonFile ${CMAKE_CURRENT_BINARY_DIR}/${target}.json)
+    if(${specFile} IS_NEWER_THAN ${jsonFile})
+        qt_get_binary_path(binaryPath)
+        execute_process(COMMAND ${binaryPath}/xmlpatterns
+            -no-format -output ${jsonFile} ${CMAKE_SOURCE_DIR}/src/pluginjsonmetadata.xsl ${specFile}
+        )
+    endif()
+endfunction()
